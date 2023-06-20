3d8319ec1185c0af8f69b044deddf5ca674c5daf
PyrusSmal <121591537+PyrusSmal@users.noreply.github.com>
2023-06-20 13:47:20 +0800

    Add files via upload

---
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -0,0 +1,117 @@
+package com.example;
+
+import com.example.common.utils.UUIDUtil;
+import com.example.dao.ProjectEntityMapper;
+import com.example.dao.UserEntityMapper;
+import com.example.dao.entity.ProjectEntity;
+import com.example.dao.entity.UserEntity;
+import com.example.service.UserService;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+
+class DemoApplicationTests {
+    private SqlSession sqlSession;
+    Logger log = Logger.getLogger(DemoApplicationTests.class);
+    @BeforeEach
+    public void setUp() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        sqlSession.close();
+    }
+
+    @Test
+    public void queryUserList() throws Exception {
+
+        UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
+        //调用userMapper的方法
+        UserEntity userEntity = new UserEntity();
+        List<UserEntity> list = userEntityMapper.queryUserList(userEntity);
+        if(CollectionUtils.isEmpty(list)){
+            // 记录error级别的信息
+        }else{
+            System.out.println(list);
+            // 记录info级别的信息
+            log.info(">>queryUserList用户列表查询测试成功");
+        }
+    }
+    @Test
+    public void selectUserInfo() throws Exception {
+
+        //创建UserMapper对象，mybatis自动生成mapper代理对象
+        UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
+        //调用userMapper的方法
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername("1");
+        userEntity.setPassword("1");
+        List<UserEntity> list = userEntityMapper.selectUserInfo(userEntity);
+        if(CollectionUtils.isEmpty(list)){
+            // 记录error级别的信息
+        }else{
+            System.out.println(list);
+            // 记录info级别的信息
+            log.info(">>qselectUserInfo用户登录测试成功");
+        }
+    }
+    @Test
+    public void insert() throws Exception {
+
+        //创建UserMapper对象，mybatis自动生成mapper代理对象
+        UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
+        //调用userMapper的方法
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setId(UUIDUtil.getUUID());
+        userEntity.setStatus("1");
+        userEntity.setUsername("LS");
+        userEntity.setPassword("123");
+
+        int i = userEntityMapper.insert(userEntity);
+        if(i==0){
+            // 记录error级别的信息
+        }else{
+            System.out.println(i);
+            // 记录info级别的信息
+            log.info(">>insert用户插入测试成功");
+        }
+    }
+
+
+    @Test
+    public void deleteUserByName() throws Exception {
+
+        //创建UserMapper对象，mybatis自动生成mapper代理对象
+        UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
+        //调用userMapper的方法
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername("000");
+        int i = userEntityMapper.deleteUserByName(userEntity);
+        if(i==0){
+            // 记录error级别的信息
+        }else{
+            System.out.println(i);
+            // 记录info级别的信息
+            log.info(">>delete用户删除测试成功");
+        }
+    }
+
+}
